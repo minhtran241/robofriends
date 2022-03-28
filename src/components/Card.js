2d4841e06fd9f561207314d40a9dad6652d0a6ad
Minh Tran <98200886+minhtran241@users.noreply.github.com>
2022-03-28 12:50:02 -0400

    Error Boundary added

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Card = ({ name, email, id }) => {
 	return (
-		<div className="tc bg-lightest-grey dib br3 pa3 ma2 grow bw2 shadow-5">
+		<div className="tc grow bg-lightest-grey br3 pa3 ma2 dib bw2 shadow-5">
 			<img alt="robots" src={`https://robohash.org//${id}?200x200`} />
 			<div>
 				<h2>{name}</h2>
