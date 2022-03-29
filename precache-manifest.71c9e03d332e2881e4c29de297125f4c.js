f78821549298e60c77f0af24d88b7bc79f2d111a
Minh Tran <98200886+minhtran241@users.noreply.github.com>
2022-03-29 07:24:00 -0400

    Updates

---
+++ precache-manifest.71c9e03d332e2881e4c29de297125f4c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "608ea5cbb42e6be9afb0",
+    "url": "/robofriends/static/css/main.b2adb331.chunk.css"
+  },
+  {
+    "revision": "608ea5cbb42e6be9afb0",
+    "url": "/robofriends/static/js/main.9352bc00.chunk.js"
+  },
+  {
+    "revision": "132d09c1690cde03bf4b",
+    "url": "/robofriends/static/js/runtime~main.f09e04fd.js"
+  },
+  {
+    "revision": "0bec9f4924068d7313d1",
+    "url": "/robofriends/static/css/2.f0b412c5.chunk.css"
+  },
+  {
+    "revision": "0bec9f4924068d7313d1",
+    "url": "/robofriends/static/js/2.f2c8162a.chunk.js"
+  },
+  {
+    "revision": "c0c98a6c240aa4e0d888",
+    "url": "/robofriends/static/js/3.68b36b5f.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "d6644a57babdbcc1cfb58864e824c006",
+    "url": "/robofriends/index.html"
+  }
+];
